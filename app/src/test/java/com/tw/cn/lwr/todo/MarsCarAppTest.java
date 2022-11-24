15c656e07df6fe3239bfcd53e2e85d64f67980d7
ray-liu233 <2621886053@qq.com>
2022-11-25 00:58:00 +0800

    test: add get car to list test

--- app/src/test/java/com/tw/cn/lwr/todo/MarsCarAppTest.java
+++ app/src/test/java/com/tw/cn/lwr/todo/MarsCarAppTest.java
@@ -1,10 +1,22 @@
 package com.tw.cn.lwr.todo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 class MarsCarAppTest {
+    @Test
+    void should_create_MarsCar_instance_when_car_coordinate_and_instruction_are_correct() {
+        MarsCar marsCar = new MarsCar(1,2,"N");
+        Scanner scanner=new Scanner(System.in);
+        String carInstructions="LMLMLMLMM";
+        List<MarsCar> carList=new ArrayList<>();
+        MarsCarApp.getCarToList(marsCar,carList,carInstructions,scanner);
+        Assertions.assertEquals(1,carList.size());
+    }
 
 
 }
