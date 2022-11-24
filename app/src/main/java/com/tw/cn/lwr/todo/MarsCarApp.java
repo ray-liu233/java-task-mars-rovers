bd5c9b1e3ca89acb86043e1db519c2dfb0984769
ray-liu233 <2621886053@qq.com>
2022-11-24 17:05:19 +0800

    chore: delete default function and test by gradle

---
+++ app/src/main/java/com/tw/cn/lwr/todo/MarsCarApp.java
@@ -0,0 +1,8 @@
+package com.tw.cn.lwr.todo;
+
+public class MarsCarApp {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ app/src/test/java/com/tw/cn/lwr/todo/MarsCarAppTest.java
@@ -0,0 +1,10 @@
+package com.tw.cn.lwr.todo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MarsCarAppTest {
+
+
+}
