8abf507cc63b8972a00de3b5168a4df5ec34cd1d
ray-liu233 <2621886053@qq.com>
2022-11-25 10:12:47 +0800

    test: add Mars car app test

--- app/src/test/java/com/tw/cn/lwr/todo/MarsCarAppTest.java
+++ app/src/test/java/com/tw/cn/lwr/todo/MarsCarAppTest.java
@@ -2,12 +2,12 @@ package com.tw.cn.lwr.todo;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 class MarsCarAppTest {
+
     @Test
     void should_create_MarsCar_instance_when_car_coordinate_and_instruction_are_correct() {
         MarsCar marsCar = new MarsCar(1,2,"N");
@@ -16,7 +16,30 @@ class MarsCarAppTest {
         List<MarsCar> carList=new ArrayList<>();
         MarsCarApp.getCarToList(marsCar,carList,carInstructions,scanner);
         Assertions.assertEquals(1,carList.size());
+        Assertions.assertEquals(1,marsCar.getCoordinateX());
+        Assertions.assertEquals(3,marsCar.getCoordinateY());
+        Assertions.assertEquals("N",marsCar.getDirection());
+
     }
 
-
+    @Test
+    void should_return_correct_coordinate_answer_when_input_car_list(){
+    MarsCar marsCar=new MarsCar(2,3,"E");
+    MarsCar marsCar2=new MarsCar(1,2,"N");
+    List<MarsCar> carList=new ArrayList<>();
+    carList.add(marsCar);
+    carList.add(marsCar2);
+        for (int i = 0; i < carList.size(); i++) {
+            if(i==0){
+                Assertions.assertEquals(2, carList.get(i).getCoordinateX());
+                Assertions.assertEquals(3, carList.get(i).getCoordinateY());
+                Assertions.assertEquals("E", carList.get(i).getDirection());
+            }
+            if(i==1){
+                Assertions.assertEquals(1, carList.get(i).getCoordinateX());
+                Assertions.assertEquals(2, carList.get(i).getCoordinateY());
+                Assertions.assertEquals("N", carList.get(i).getDirection());
+            }
+        }
+    }
 }
